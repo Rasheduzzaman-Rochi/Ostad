acaf503d539b3be5d5a799f62d42c6cafe6f7bde
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-11-15 23:59:20 +0600

    Second_Assignment

--- lib/main.dart
+++ lib/main.dart
@@ -18,91 +18,99 @@ class MyProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Profile'),
-        toolbarHeight: 60,
-        backgroundColor: Colors.amber,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.call),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                SizedBox(height: 10),
-                CircleAvatar(
-                  radius: 80,
-                  backgroundColor: Colors.deepPurpleAccent[50],
-                  child: Icon(
-                    Icons.icecream_outlined,
-                    size: 90,
-                    color: Colors.indigoAccent[800],
-                  ),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  'Ice cream is very delicious right?',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18, color: Colors.black,fontWeight: FontWeight.bold),
-                ),
-              ],
+        appBar: AppBar(
+          title: Text('My Profile'),
+          toolbarHeight: 60,
+          backgroundColor: Colors.amber,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
             ),
-            SizedBox(height: 30),
-            Column(
-              children: [
-                CircleAvatar(
-                  radius: 80,
-                  backgroundColor: Colors.deepPurpleAccent[50],
-                  child: Icon(
-                    Icons.code,
-                    size: 90,
-                    color: Colors.indigoAccent[800],
-                  ),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  'Programming is not boring if you love it',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18, color: Colors.black,fontWeight: FontWeight.bold),
-                ),
-              ],
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
             ),
-            SizedBox(height: 30),
-            Column(
-              children: [
-                CircleAvatar(
-                  radius: 80,
-                  backgroundColor: Colors.deepPurpleAccent[50],
-                  child: Icon(
-                    Icons.egg_outlined,
-                    size: 90,
-                    color: Colors.indigoAccent[800],
-                  ),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  'If you submit code directly copy from Chatgpt then mark will 0',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(fontSize: 18, color: Colors.black,fontWeight: FontWeight.bold),
-                ),
-              ],
+            IconButton(
+              icon: Icon(Icons.call),
+              onPressed: () {},
             ),
           ],
         ),
-      ),
-    );
+        body: Container(
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  SizedBox(height: 10),
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Colors.deepPurpleAccent[50],
+                    child: Icon(
+                      Icons.icecream_outlined,
+                      size: 90,
+                      color: Colors.indigoAccent[800],
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Ice cream is very delicious right?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Column(
+                children: [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Colors.deepPurpleAccent[50],
+                    child: Icon(
+                      Icons.code,
+                      size: 90,
+                      color: Colors.indigoAccent[800],
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Programming is not boring if you love it',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Column(
+                children: [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Colors.deepPurpleAccent[50],
+                    child: Icon(
+                      Icons.egg_outlined,
+                      size: 90,
+                      color: Colors.indigoAccent[800],
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'If you submit code directly copy from Chatgpt then mark will 0',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
